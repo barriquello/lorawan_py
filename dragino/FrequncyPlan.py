8c57c6a089dce65d225a0627edbcfbcaf8edcc31
barriquello <barriquello@gmail.com>
2018-12-18 09:50:42 -0200

    1st commit - working

---
+++ dragino/FrequncyPlan.py
@@ -0,0 +1,29 @@
+###############################################################################
+#Details of the frequncies available for LoRaWAN usage
+#Copyright (C) 2018 Philip Basford
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
+
+#https://www.thethingsnetwork.org/wiki/LoRaWAN/Frequencies/Frequency-Plans
+CH1 = 916.8
+CH2 = 917.0
+CH3 = 917.2
+CH4 = 917.4
+CH5 = 917.6
+CH6 = 917.8
+CH7 = 918.0
+CH8 = 918.2
+
+LORA_FREQS = (CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8)
---
+++ test2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Test harness for dragino module - sends hello world out over LoRaWAN 5 times
+"""
+import logging
+from time import sleep
+import RPi.GPIO as GPIO
+from dragino import Dragino
+
+
+GPIO.setwarnings(False)
+
+D = Dragino("dragino2.ini", logging_level=logging.DEBUG)
+#D.join()
+#while not D.registered():
+#    print("Waiting")
+#    sleep(2)
+#sleep(10)
+#for i in range(0, 5):
+while 1:
+    D.send("Hello World")
+    print("Sent message")
+    sleep(10)
